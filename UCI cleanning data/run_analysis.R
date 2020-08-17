6685ff833cd7b9f8349941039ab1a27723f48466
joc2zan <31548862+joc2zan@users.noreply.github.com>
2020-08-17 09:21:50 -0500

    Add files via upload

--- UCI cleanning data/run_analysis.R
+++ UCI cleanning data/run_analysis.R
@@ -1,6 +1,6 @@
 # Set working directory
 
-setwd("./UCI cleanning data/UCI HAR Dataset/")
+setwd("../UCI cleanning data/UCI cleanning data/UCI HAR Dataset/")
 
 
 
@@ -13,23 +13,24 @@ str_url_folder_train<-"./train/"
 f<-paste(str_url_folder_train,"subject_train.txt",sep = "")
 con_subject_train<-file(f,"rt")
 
-subject_train<-read.table(file = con_subject_train,header = T, col.names = "subject_train" )
+subject_train<-read.table(file = con_subject_train,header = T, col.names = "sub_id" )
 close(con_subject_train)
 
 x_train<-read.table(file = "./train/X_train.txt",header = T )
 colnames(x_train)<-name_cols[,2]
 
-y_train<-read.table(file = "./train/y_train.txt",header = T, col.names = "y_train")
+y_train<-read.table(file = "./train/y_train.txt",header = T, col.names = "activity")
 
 
 ######## Read all docs Test ###########################
 
-subject_test<-read.table(file = "./test/subject_test.txt",header = T, col.names = "subject_test" )
+subject_test<-read.table(file = "./test/subject_test.txt",header = T, col.names = "sub_id" )
 
 x_test<-read.table(file = "./test/X_test.txt",header = T)
 colnames(x_test)<-name_cols[,2]
 
-y_test<-read.table(file = "./test/y_test.txt",header = T,col.names = "y_test" )
+y_test<-read.table(file = "./test/y_test.txt",header = T,col.names = "activity" )
+
 
 
 ########----TEST------------------------------------------------------------
@@ -104,26 +105,18 @@ colnames(total_acc_z_train)<-paste("total_acc_z_train_",seq(1:length(total_acc_z
 #-----------------------------#############  MERGE  ######################--------------------------------------------
 
 #marge subjects x, y (test vs train)
-n_test<-colnames(subject_test)
-colnames(subject_train)<-n_test
-colnames(subject_train)
-colnames(subject_test)
-s1<-rbind(subject_train,subject_test)
 
-#data.table(subject_test,subject_train)
+s1<-cbind(subject_train,x_train,y_train)
+head(s1)
 
-#n_test<-colnames(subject_test)
-#colnames(subject_train)<-n_test
-#rbind(subject_train,subject_test)
+s2<-cbind(subject_test,x_test,y_test)
+head(s2)
 
-#s2<-data.frame(x_test,x_train)
-s2<-rbind(x_test,x_train)
+#colnames(s2)==colnames(s1)
+dataset_coursera<-rbind(s1,s2)
+head(dataset_coursera)
 
-#s3<-data.frame(y_test,y_train)
-n_test<-colnames(y_test)
-colnames(y_train)<-n_test
-s3<-rbind(y_test,y_train)
-
+############################################################################
 #merge body_acc x,y,z (test vs train) 
 n_test<-colnames(body_acc_x_test)
 colnames(body_acc_x_train)<-n_test
@@ -175,23 +168,29 @@ s12<-rbind(total_acc_z_train,total_acc_z_test)
 
 
 #Dataset with names from the source
-dataset_coursera<-data.frame(s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12)
+#dataset_coursera<-data.frame(s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12)
 #View(dataset_coursera)
 
 
 
 #Dataset with colnames from features.txt
-dataset_coursera2<-data.frame(s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12)
+#dataset_coursera2<-data.frame(s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12)
 
 
 ###--------------------------------------measurements
 
-n<-grep(pattern = "*mean",x = colnames(dataset_coursera2))
-measurements_mean<-dataset_coursera2[,n]
+n<-grep(pattern = "*mean|*std|sub_id|activity",x = colnames(dataset_coursera))
+measurements_mean_std<-dataset_coursera[,n]
+head(measurements_mean_std)
 
-n<-grep(pattern = "*std",x = colnames(dataset_coursera2))
-measurements_std<-dataset_coursera2[,n]
+final_data<-transform(measurements_mean_std, activity=factor(activity, levels=c("1", "2", "3", "4", "5", "6"),
+                                      labels=c("walking", "walkingupstairs", 
+                                               "walkingdownstairs", "sitting",
+                                               "standing", "laying")))
+head(final_data)
 
+by_activity<-group_by(final_data, activity,sub_id)
+summarise_all(by_activity,mean)
 ##---------------------------------------Final Dataset 
 #View(dataset_coursera2)
-write.table(x = dataset_coursera2,file = "./final.txt",row.names = FALSE)
+write.table(x = by_activity,file = "./final.txt",row.names = FALSE)
